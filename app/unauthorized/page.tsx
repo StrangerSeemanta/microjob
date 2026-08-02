bd32b2906442f6b6a42bf314297f7d722ad05ebc
StrangerSeemanta <ssworkmail22@gmail.com>
2026-08-03 04:04:12 +0600

    first commit

--- app/unauthorized/page.tsx
+++ app/unauthorized/page.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import { currentUser } from "@clerk/nextjs/server";
 
-const user = await currentUser();
 
-console.log(user?.publicMetadata);
 async function page() {
   return (
     <div className="flex justify-center items-center flex-col min-h-screen w-full">Unauthorized Attempt detected !!! <h1 className="text-xl font-bold text-red-500">You are not authorized to access this page.</h1></div>
