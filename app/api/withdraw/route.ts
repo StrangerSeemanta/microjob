11af7604e9a4441a852bd031f923186b18839f30
StrangerSeemanta <ssworkmail22@gmail.com>
2026-08-06 02:47:24 +0600

    fixed i

--- app/api/withdraw/route.ts
+++ app/api/withdraw/route.ts
@@ -93,7 +93,6 @@ export async function POST(req: NextRequest) {
     //---------------------------------------
 
     const withdrawal = await WithdrawalRequest.create({
-      userId: user._id.toString(),
       clerkId,
 
       amount,
